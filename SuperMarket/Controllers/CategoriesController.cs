0ac56c9af5d061cfa3cdc71fda1fa99863467e21
Kavinda Dissanayaka <92946@fhss.sjp.ac.lk>
2024-05-12 17:16:07 +0530

    CategoriesModel added and add Edit action method

---
+++ SuperMarket/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using SuperMarket.Models;
+
+namespace SuperMarket.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var myCatagory = new CategoriesModel { CategoryID = id};
+            return View(myCatagory);
+        }
+    }
+}
---
+++ SuperMarket/Models/CategoriesModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperMarket.Models
+{
+    public class CategoriesModel
+    {
+        public Int32 CategoryID { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string CategoryDescription { get; set; }= string.Empty;
+    }
+}
